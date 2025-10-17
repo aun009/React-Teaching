03120e6b64a3c226a1c20b2a406a6a873acec728
aun009 <arunmahajan9240@gmail.com>
2025-10-17 09:27:50 +0530

    started with project

--- 05-form-handling/src/App.jsx
+++ 05-form-handling/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [title, setTitle] = useState('')
+
+  const formSubmitted = (e)=> {
+    e.preventDefault()
+    console.log("form submitted by ",  title)
+    setTitle('')
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='h-screen  flex-col bg-black'>
+      <form>
+        <input type="text" className='text-white p-5 mt-4 ml-6 border-2' value = {title} onChange={(e)=> {
+          setTitle(e.target.value)
+          console.log(e.target.value)
+        }}  placeholder='Enter name'/>
+        <button onClick={(e)=> {
+          formSubmitted(e)
+        }} className='text-white ml-30 '>Submit</button>
+      </form>text
+    </div>
   )
 }
 
 export default App
+
+
+// now two way binding, ek tir se 2 nishan
